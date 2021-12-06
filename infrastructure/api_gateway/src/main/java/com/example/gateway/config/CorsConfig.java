bfcf7f5501e2d72434c38f7a2b10ea93e338ea38
Layne <1984425838@qq.com>
2021-12-06 21:34:58 +0800

    11 commit

---
+++ infrastructure/api_gateway/src/main/java/com/example/gateway/config/CorsConfig.java
@@ -0,0 +1,30 @@
+package com.example.gateway.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+import org.springframework.web.util.pattern.PathPatternParser;
+
+/**
+ * description:
+ *      使用网关解决跨域问题，但是使用这个就不能加 @CrossOrigin 注解
+ * @author wangpeng
+ * @date 2019/01/02
+ */
+@Configuration
+public class CorsConfig {
+    @Bean
+    public CorsWebFilter corsFilter() {
+        CorsConfiguration config = new CorsConfiguration();
+        config.addAllowedMethod("*");
+        config.addAllowedOrigin("*");
+        config.addAllowedHeader("*");
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource(new PathPatternParser());
+        source.registerCorsConfiguration("/**", config);
+
+        return new CorsWebFilter(source);
+    }
+}
